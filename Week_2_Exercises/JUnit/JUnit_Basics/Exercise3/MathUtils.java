351f8346f749887df5eb2d66e619d92c988e1070
Madhumiga_Vijayakumar <124855723+MADHUMIGA@users.noreply.github.com>
2025-06-28 09:09:34 +0530

    Create MathUtils.java

---
+++ Week_2_Exercises/JUnit/JUnit_Basics/Exercise3/MathUtils.java
@@ -0,0 +1,20 @@
+package in.madhu.junit_practice;
+
+public class MathUtils {
+       	  public int add(int a, int b) {
+    	        return a + b;
+    	    }
+
+    	    public int divide(int a, int b) {
+    	        return a / b;
+    	    }
+
+    	    public boolean isEven(int number) {
+    	        return number % 2 == 0;
+    	    }
+
+    	    public String getWelcomeMessage() {
+    	        return "Welcome to JUnit!";
+    	    }
+    
+}
