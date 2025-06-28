025ca01002af474130d45ca53a068c4df82225e9
Madhumiga_Vijayakumar <124855723+MADHUMIGA@users.noreply.github.com>
2025-06-28 21:53:51 +0530

    Update Scenario2.java

--- Week_2_Exercises/PL-SQL/StoredProcedure/Scenario2.java
+++ Week_2_Exercises/PL-SQL/StoredProcedure/Scenario2.java
@@ -1,1 +1,14 @@
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+  deptId IN NUMBER,
+  bonusPercent IN NUMBER
+) AS
+BEGIN
+  UPDATE Employees
+  SET Salary = Salary + (Salary * bonusPercent / 100)
+  WHERE DepartmentID = deptId;
+
+  COMMIT;
+END;
+
+EXEC UpdateEmployeeBonus(10, 5);  -- Adds 5% bonus to employees in dept 10
 
