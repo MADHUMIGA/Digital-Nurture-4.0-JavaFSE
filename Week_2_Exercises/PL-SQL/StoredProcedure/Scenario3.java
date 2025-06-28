4bf4ee617b6d22993f86e493aa03153783e308cd
Madhumiga_Vijayakumar <124855723+MADHUMIGA@users.noreply.github.com>
2025-06-28 21:54:20 +0530

    Update Scenario3.java

--- Week_2_Exercises/PL-SQL/StoredProcedure/Scenario3.java
+++ Week_2_Exercises/PL-SQL/StoredProcedure/Scenario3.java
@@ -1,1 +1,33 @@
+CREATE OR REPLACE PROCEDURE TransferFunds(
+  fromAccount IN NUMBER,
+  toAccount IN NUMBER,
+  amount IN NUMBER
+) AS
+  currentBalance NUMBER;
+BEGIN
+  -- Get balance of source account
+  SELECT Balance INTO currentBalance
+  FROM Accounts
+  WHERE AccountID = fromAccount;
+
+  -- Check if sufficient balance
+  IF currentBalance < amount THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Insufficient balance in source account.');
+  END IF;
+
+  -- Deduct from source
+  UPDATE Accounts
+  SET Balance = Balance - amount
+  WHERE AccountID = fromAccount;
+
+  -- Add to destination
+  UPDATE Accounts
+  SET Balance = Balance + amount
+  WHERE AccountID = toAccount;
+
+  COMMIT;
+END;
+
+
+EXEC TransferFunds(101, 202, 500);
 
