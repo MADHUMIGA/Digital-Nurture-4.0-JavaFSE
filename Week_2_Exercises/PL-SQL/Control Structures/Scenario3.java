08b7a5a156a302b4ca6c9d38b8f0064b5f501612
Madhumiga_Vijayakumar <124855723+MADHUMIGA@users.noreply.github.com>
2025-06-28 21:49:17 +0530

    Update Scenario3.java

--- Week_2_Exercises/PL-SQL/Control Structures/Scenario3.java
+++ Week_2_Exercises/PL-SQL/Control Structures/Scenario3.java
@@ -1,1 +1,10 @@
+BEGIN
+  FOR rec IN (
+    SELECT CustomerID, DueDate FROM Loans 
+    WHERE DueDate <= SYSDATE + 30
+  ) LOOP
+    DBMS_OUTPUT.PUT_LINE('Reminder: Customer ' || rec.CustomerID ||
+                         ' has a loan due on ' || TO_CHAR(rec.DueDate, 'DD-MON-YYYY'));
+  END LOOP;
+END;
 
